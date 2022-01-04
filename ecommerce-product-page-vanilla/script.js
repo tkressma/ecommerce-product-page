724fbb7bb156b17ce00e3227b4b2a909ad18ce99
Tom Kressman <tkressma@oswego.edu>
2022-01-04 15:51:03 -0500

    Finish cart functionality

--- ecommerce-product-page-vanilla/script.js
+++ ecommerce-product-page-vanilla/script.js
@@ -4,24 +4,25 @@ const itemCounter = document.querySelector("#product__item-amount");
 const itemCartDisplay = document.querySelector("#cart__item-count");
 const addToCart = document.querySelector(".product__add-to-cart");
 const cartIcon = document.querySelector("#cart__open");
+const cartItem = document.querySelector(".cart__item");
 const cartContents = document.querySelector(".cart__contents");
+const cartCheckout = document.querySelector(".cart__checkout");
+const cartEmptyMsg = document.querySelector(".cart__empty");
 let itemCountBuffer = Number(itemCartDisplay.innerHTML);
 let itemCountDisplay = 0;
 
-const cartItem =
-  // Item Add/Remove
-  document
-    .querySelector(".product__add-items")
-    .addEventListener("click", (e) => {
-      if (e.target.id === "higher" || e.target.id === "higher-icon") {
-        itemCounter.innerHTML = ++itemCountBuffer;
-      }
+// Item Add/Remove
+document.querySelector(".product__add-items").addEventListener("click", (e) => {
+  if (e.target.id === "higher" || e.target.id === "higher-icon") {
+    itemCounter.innerHTML = ++itemCountBuffer;
+  }
 
-      if (e.target.id === "lower" || e.target.id === "lower-icon") {
-        itemCounter.innerHTML = itemCountBuffer > 0 ? --itemCountBuffer : 0;
-      }
-    });
+  if (e.target.id === "lower" || e.target.id === "lower-icon") {
+    itemCounter.innerHTML = itemCountBuffer > 0 ? --itemCountBuffer : 0;
+  }
+});
 
+// User adds items to cart
 addToCart.addEventListener("click", () => {
   itemCountDisplay = itemCountDisplay + itemCountBuffer;
   if (itemCountDisplay === 0) return;
@@ -29,22 +30,46 @@ addToCart.addEventListener("click", () => {
   if (itemCartDisplay.classList.contains("hidden")) {
     itemCartDisplay.classList.remove("hidden");
     itemCartDisplay.innerHTML = itemCountDisplay;
-    updateCartContents();
+    updateCartContents(false);
     return;
   }
+
   itemCartDisplay.innerHTML = itemCountDisplay;
-  updateCartContents();
 });
 
+// Display the cart when a user clicks on the icon
 cartIcon.addEventListener("click", () => {
   document.querySelector(".cart").classList.toggle("hidden");
 });
 
-const updateCartContents = () => {
-  cartContents.insertAdjacentHTML(
-    "afterbegin",
-    "<img src='./images/image-product-1-thumbnail.jpg' alt='' class='cart__product-img' /> <div class='cart__product-info'> <p>Product title</p> <p>Placeholder Value $XXX</p> <button class='cart__delete-item'> <img src='./images/icon-delete.svg' alt='' /> </button> </div> </div><button class='cart__checkout'>Checkout</button>"
-  );
+// Handles adding and removing items from the cart display
+const updateCartContents = (emptyCart) => {
+  emptyCart ? removeItemsFromCart() : displayItemsInCart();
+};
 
-  document.querySelector(".cart__empty").remove();
+// Displays the items in the car when a user adds them
+const displayItemsInCart = () => {
+  cartItem.innerHTML = `<img src='./images/image-product-1-thumbnail.jpg' alt='' class='cart__product-img' /> <div class='cart__product-info'> <p>Autumn Limited Edition...</p> <p>$125.00 x ${itemCountDisplay} <b>$${
+    125 * itemCountDisplay
+  }</b></p> </div> <button class='cart__delete-item'> <img src='./images/icon-delete.svg' alt='' /> </button> </div>`;
+
+  cartEmptyMsg.innerHTML = "";
+  cartContents.classList.remove("empty");
+  cartCheckout.classList.remove("hidden");
+
+  // When a user clicks on the delete icon, remove all items from the cart
+  document
+    .querySelector(".cart__delete-item")
+    .addEventListener("click", () => updateCartContents(true));
+  return;
+};
+
+// Removes the items from the cart
+const removeItemsFromCart = () => {
+  cartItem.innerHTML = "";
+  cartEmptyMsg.innerHTML = "Your cart is empty.";
+  cartContents.classList.add("empty");
+  cartCheckout.classList.add("hidden");
+  itemCartDisplay.classList.add("hidden");
+  itemCountDisplay = 0;
 };
